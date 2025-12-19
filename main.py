ec6edf17c6e6153a86714889a654587d47b541f0
noahchampok-cmyk <noahchampok@gmail.com>
2025-12-19 12:24:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ line_bot_api = LineBotApi(LINE_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_SECRET)
 notion = Client(auth=NOTION_TOKEN)
 genai.configure(api_key=GEMINI_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash-001')
+model = genai.GenerativeModel('gemini-pro')
 
 def ask_gemini(user_text):
     prompt = f"""
